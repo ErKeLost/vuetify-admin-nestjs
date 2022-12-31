84003a4b8ab3293632d88a301622b28d95679724
ERKELSOT <1256029807@qq.com>
2022-12-31 16:46:06 +0800

    chore: configure with webpack hot reload

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
