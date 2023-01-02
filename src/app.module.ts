b354e03eac07c91a1587c4aa2458eb709227d520
ERKELSOT <1256029807@qq.com>
2023-01-02 11:14:21 +0800

    chore： env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule],
+  // forRoot 读取 .env 文件
+  imports: [
+    ConfigModule.forRoot({
+      // 全局模块都能使用 不然只有app模块可以使用
+      isGlobal: true,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
