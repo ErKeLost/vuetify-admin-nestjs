25d228d66322a1e7945f28fd617da6e98b656f33
erkelost <1256029807@qq.com>
2023-03-22 09:43:30 +0800

    feat: add logs entity

---
+++ src/logs/entities/logs.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+
+@Entity()
+export class Logs {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  path: string;
+
+  @Column()
+  methods: string;
+
+  @Column()
+  data: string;
+
+  @Column()
+  result: string;
+
+  @ManyToOne(() => User, (user) => user.logs)
+  @JoinColumn()
+  user: User;
+}
